9b050af90f8a4cdfc510b71ea272d09390b4c607
Arslan Sarfraz <arslansarfraz114@gmail.com>
2022-09-02 11:23:00 +0000

    Update 4 files

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import "./styles.css";
 //Imports Components
 import Header from "./Components/Header";
 import { Balance } from "./Components/Balance";
+import AccountSummry from "./Components/AccountSummry";
 
 const App = () => {
   return (
     <div className="app">
       <Header />
       <Balance />
+      <AccountSummry />
     </div>
   );
 };
--- src/Components/Balance.js
+++ src/Components/Balance.js
@@ -3,7 +3,7 @@ import React from "react";
 export const Balance = () => {
   return (
     <div className="textLeft pt-50">
-      <h3>YOURE BALANCE:</h3>
+      <h3>YOURE BALANCE IS:</h3>
       <h2 className="fweight">Rs: 1234</h2>
     </div>
   );
