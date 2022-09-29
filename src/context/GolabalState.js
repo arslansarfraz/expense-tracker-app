391cbd6466ba3b242c4eff6e8b1abec7f51dc70b
Arslan Sarfraz <arslansarfraz114@gmail.com>
2022-09-29 21:01:06 +0000

    Update GolabalState.js

--- src/context/GolabalState.js
+++ src/context/GolabalState.js
@@ -1,3 +1,27 @@
 import React, { createContext, useReducer } from 'react';
+import AppReducer from './AppReducer';
+// Create the Inital State
+const initalState = {
+    transaction: [
+        { id: 1, description: 'Income 1', transactionAmount: 100 },
+        { id: 2, description: 'Expense 2', transactionAmount: -200 },
+        { id: 3, description: 'Income 3', transactionAmount: 300 },
+        { id: 4, description: 'Expense 4', transactionAmount: -400 }
+    ]
+}
+
+// Create the Global Context
+export const GlobalContext = createContext(initalState)
 
 
+export const GlobalProvider = (children) => {
+
+    const [state, dispatch] = useReducer(AppReducer, initalState)
+    return (
+        <GlobalContext.Provider value={
+            { transaction: state.transaction }
+        }>
+            {children}
+        </GlobalContext.Provider >
+    )
+}
